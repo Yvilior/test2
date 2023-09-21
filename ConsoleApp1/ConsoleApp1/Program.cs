a94a90203256c04b4d7f04b9325323831c0fbfec
Guillon Yvain <yvainmacguire@icloud.com>
2023-09-21 11:30:28 -0400

    programme

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration des variables
+            string nom;
+            double C, F;
+            //Demander nom
+            Console.WriteLine("Quel est votre nom?");
+            nom = Console.ReadLine();
+            Console.WriteLine("Bienvenue " + nom + "!");
+            //Demander la temperature
+            Nb1:
+            Console.WriteLine("Quel est la temperature que vous recherchez?");
+            try
+            {
+                C = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex1)
+            {
+
+                Console.WriteLine("Votre temperature doit etre un nombre");
+                goto Nb1;
+
+            }
+            //Convertion de la temperature
+            F = C * (9 / 5) + 32;
+            Console.WriteLine("Votre temperature en Fahrenheit est: " + F + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
